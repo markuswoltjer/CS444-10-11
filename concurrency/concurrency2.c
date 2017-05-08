9ee7ed312a93b19d8c66fe4f2b43d16e9983babb
Markus Woltjer <markuswoltjer@gmail.com>
2017-05-08 14:02:11 -0700

    added all fork statuses at any state change

--- concurrency/concurrency2.c
+++ concurrency/concurrency2.c
@@ -3,26 +3,30 @@
 #include <time.h>
 #include <unistd.h>
 #include <omp.h>
-
-#define NUM_PHILOSOPHERS 5
+#include "mt19937ar.c"
 
 typedef int bool;
 #define true 1
 #define false 0
+#define NUM_PHILOSOPHERS 5
 
+int Forks[5];
 static omp_lock_t forks[NUM_PHILOSOPHERS];
 
-void philosopher() {
+//add five philoshers global array here
 
+void philosopher() {
 	#pragma omp barrier
+	
+	
 	
 	int id = omp_get_thread_num();
 	int left_fork;
 	int right_fork;
-
-	//1 to 20 seconds of thinking
+	
 	srand(time(NULL));
-	sleep((rand() % 20) + 1);
+	
+	sleep((rand() % 20) + 1); //Waiting/Thinking is between 1 and 20 seconds
 	
 	if (id < NUM_PHILOSOPHERS - 1) {
 		left_fork = id + 1;
@@ -36,17 +40,32 @@ void philosopher() {
 	
 	omp_set_lock(&forks[left_fork]);
 	omp_set_lock(&forks[right_fork]);
-
-	printf("Philosopher with id %d is eating today.\n", id);
-	printf("In his left hand is fork %d and in his right hand is fork %d.\n", left_fork, right_fork);
+	
+	Forks[left_fork] = 0;
+	Forks[right_fork] = 0;
+	
+	printf("forks %d and %d were picked up\n", left_fork, right_fork);
+	
+	printf("Fork status: 0 = unavailable, 1 = available\n");
+	printf("Fork 0: %d Fork 1: %d Fork 2: %d Fork 3: %d Fork 4: %d\n\n", Forks[0], Forks[1], Forks[2], Forks[3], Forks[4]);
+	
+	printf("philosopher %d is eating\n\n", id);
 
 	sleep((rand() % 8) + 2); //Eating is between 2 and 9 seconds
 	
-	printf("Philosopher with id %d is full for now.\n", id);
+	printf("philosopher %d has finished eating\n", id);
 	
 	omp_unset_lock(&forks[left_fork]);
 	omp_unset_lock(&forks[right_fork]);
-	printf("Forks %d and %d, were put on the table.\n\n", left_fork, right_fork);
+	
+	printf("forks %d and %d, were put on the table\n\n", left_fork, right_fork);
+	
+	Forks[left_fork] = 1;
+	Forks[right_fork] = 1;
+	
+	printf("Fork status: 0 = unavailable, 1 = available\n");
+	printf("Fork 0: %d Fork 1: %d Fork 2: %d Fork 3: %d Fork 4: %d\n\n", Forks[0], Forks[1], Forks[2], Forks[3], Forks[4]);
+	
 }
 
 //openMP splits into threads to coordinate fork acquisition/placement
@@ -56,6 +75,9 @@ main() {
 	int i;
 	int done;
 	done = 0;
+	
+	for (i = 0; i < 5; i++)
+		Forks[i] = 1;
 	
 	for (i = 0; i < NUM_PHILOSOPHERS; i++)
 		omp_init_lock(&forks[i]);
